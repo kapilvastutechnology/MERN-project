f5b5c984f20ab1eb9ba300b6a90dd55d8e539cba
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-08-28 13:37:08 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router-dom";
 import RootLayOut from "./components/RootLayOut.jsx";
-import NotFound from "./pages/not-found/NotFound.jsx";
-import Projects from "./pages/home/Projects.jsx";
 
 export default function App() {
   const router = createBrowserRouter([
@@ -14,19 +12,7 @@ export default function App() {
           index: true,
           element: <Home />
         },
-
-        {
-          path: 'about',
-          element: <AboutSection/>
-        },
-        {
-          path: 'contact',
-          element: <Contact />
-        },
-        {
-          path:'page',
-          element:<Projects/>
-        }
+     
       ]
     },
     {

f9ce7b7522c4b17c740980f58294342abd49b282
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-08-28 13:30:52 +0545

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router-dom";
-import Contact from "./pages/contact/Contact.jsx";
-import Home from "./pages/home/Home.jsx";
 import RootLayOut from "./components/RootLayOut.jsx";
 import NotFound from "./pages/not-found/NotFound.jsx";
-import AboutSection from "./pages/about/AboutSection.jsx";
 import Projects from "./pages/home/Projects.jsx";
 
 export default function App() {
--- src/pages/home/ProjectImage.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function ProjectImage({ image, label }) {
-  return (
-    <div className="relative">
-      <img src={image} alt="" />
-      <h1 className="absolute top-0 text-white text-lg bg-black p-2">{label}</h1>
-    </div>
-  )
-}

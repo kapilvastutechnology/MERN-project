5e00cae19154391580dd34a70fcf76e23702e8b4
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-08-28 23:26:49 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,7 @@
 
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
-      <h1>hell</h1>
-      {/* <Button></Button> */}
-      <Button>hello everyone</Button>
     </div>
   )
 }
